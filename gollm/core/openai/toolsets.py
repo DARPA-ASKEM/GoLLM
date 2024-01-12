cc482478abb40ff5de07c9db56e91e666950a460
Julian Whiting <j2whitin@gmail.com>
2024-01-12 11:27:18 -0500

    reformat repo structure for dockerizing

--- gollm/core/openai/toolsets.py
+++
@@ -1,5 +0,0 @@
-from core.openai.tool_utils import _get_model_config, _ask_a_human
-from core.entities import Toolset, Tool
-
-ASKEMConfigDemo = Toolset([_get_model_config, _ask_a_human])
-AskHuman = Toolset([_ask_a_human])
--- gollm/setup.py
+++
@@ -1,4 +0,0 @@
-openai==1.6.1
-pydantic==2.5.3
-regex
-streamlit==1.29.0
