890147325a604935d3320cb80ebfa4752c144bf6
Julian Whiting <j2whitin@gmail.com>
2024-02-16 13:30:29 -0500

    update output schema

--- core/openai/prompts/petrinet_config.py
+++ core/openai/prompts/petrinet_config.py
@@ -3,13 +3,18 @@ PETRINET_PROMPT = """
       Use the following petri net json file as a reference: {petrinet}.
       Assume that parameter fields with missing values may have multiple different sets values discussed in the research paper for different conditions.\n
       Return the different sets of initial parameters for the petri net model file like so:
+      ```
+      json
+	  {{"conditions": {{"condition_1": "description of condition_1", "condition_2": "description of condition_2", ...}},
 	  "parameters": [{{
       "id": "beta",
       "value": {{'condition_1': 0.1, 'condition_2': 0.2}},
       ....
       }}
 	  ],
-      If a condition is not mentioned in the research paper, then the value should be set to null. Be sure to use consistent naming conventions for the conditions. Instead of 'condition_1' and 'condition_2'.. use names that are descriptive of the conditions.
+      }}
+      If a condition is not mentioned in the research paper, then the value should be set to the string "null". Be sure to use consistent naming conventions for the conditions.
+      Instead of 'condition_1' and 'condition_2'.. use names that are descriptive of the conditions.
       Only use parameters found in the reference petrinet file provided above.
       Ensure that the output follows the above petri net format and can be serialized as a JSON. Specifically populate parameters and initials. Use the following
       research paper to answer the user's query: {research_paper}\n\n Answer:
