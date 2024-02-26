e222a87eb2ea1fd20d877d05055ef82434c3f8c3
Julian Whiting <j2whitin@gmail.com>
2024-02-26 12:54:52 -0500

    utils for preprocessing

--- core/utils.py
+++ core/utils.py
@@ -4,20 +4,20 @@ import tiktoken
 
 
 def escape_newlines(text: str) -> str:
-	return text.replace("\n", "\\n")
+    return text.replace("\n", "\\n")
 
 
 def remove_references(text: str) -> str:
-	"""
-	Removes reference sections from a scientific paper.
-	"""
-	pattern = r"References\n([\s\S]*?)(?:\n\n|\Z)"
-	new_text = re.sub(pattern, '', text)
-	return new_text.strip()
+    """
+    Removes reference sections from a scientific paper.
+    """
+    pattern = r"References\n([\s\S]*?)(?:\n\n|\Z)"
+    new_text = re.sub(pattern, "", text)
+    return new_text.strip()
 
 
 def extract_json(text: str) -> dict:
-    corrected_text = text.replace('{{', '{').replace('}}', '}')
+    corrected_text = text.replace("{{", "{").replace("}}", "}")
     print(corrected_text)
     try:
         json_obj = json.loads(corrected_text)
@@ -25,41 +25,68 @@ def extract_json(text: str) -> dict:
     except json.JSONDecodeError as e:
         raise ValueError(f"Error decoding JSON: {e}\nfrom text {text}")
 
-def normalize_greek_alphabet(text: str) -> str:
-	greek_to_english = {
-		'α': 'alpha', 'β': 'beta', 'γ': 'gamma', 'δ': 'delta', 'ε': 'epsilon',
-		'ζ': 'zeta', 'η': 'eta', 'θ': 'theta', 'ι': 'iota', 'κ': 'kappa',
-		'λ': 'lambda', 'μ': 'mu', 'ν': 'nu', 'ξ': 'xi', 'ο': 'omicron',
-		'π': 'pi', 'ρ': 'rho', 'σ': 'sigma', 'τ': 'tau', 'υ': 'upsilon',
-		'φ': 'phi', 'χ': 'chi', 'ψ': 'psi', 'ω': 'omega'
-	}
 
-	normalized_text = ""
-	for char in text:
-		if char.lower() in greek_to_english:
-			normalized_text += greek_to_english[char.lower()]
-		else:
-			normalized_text += char
-	return normalized_text
+def normalize_greek_alphabet(text: str) -> str:
+    greek_to_english = {
+        "α": "alpha",
+        "β": "beta",
+        "γ": "gamma",
+        "δ": "delta",
+        "ε": "epsilon",
+        "ζ": "zeta",
+        "η": "eta",
+        "θ": "theta",
+        "ι": "iota",
+        "κ": "kappa",
+        "λ": "lambda",
+        "μ": "mu",
+        "ν": "nu",
+        "ξ": "xi",
+        "ο": "omicron",
+        "π": "pi",
+        "ρ": "rho",
+        "σ": "sigma",
+        "τ": "tau",
+        "υ": "upsilon",
+        "φ": "phi",
+        "χ": "chi",
+        "ψ": "psi",
+        "ω": "omega",
+    }
+
+    normalized_text = ""
+    for char in text:
+        if char.lower() in greek_to_english:
+            normalized_text += greek_to_english[char.lower()]
+        else:
+            normalized_text += char
+    return normalized_text
+
 
 def exceeds_tokens(prompt: str, max_tokens: int) -> bool:
-	enc = tiktoken.get_encoding("cl100k_base")
-	if len(enc.encode(prompt)) > max_tokens:
-		return True
-	return False
+    enc = tiktoken.get_encoding("cl100k_base")
+    if len(enc.encode(prompt)) > max_tokens:
+        return True
+    return False
+
 
 def model_config_adapter(model_config: dict) -> dict:
+    """
+    Adapter function which converts the model config dict to HMI expected format.
+    """
 
-	"""
-	Adapter function which converts the model config dict to HMI expected format.
-	"""
-
-	output_json = {'conditions': []}
-	print(model_config)
-	for condition_name, description in model_config['conditions'].items():
-		condition_data = {'name': condition_name, 'description': description, 'parameters': []}
-		for param_data in model_config['parameters']:
-			param_value = param_data['value'].get(condition_name)
-			condition_data['parameters'].append({'id': param_data['id'], 'value': param_value})
-		output_json['conditions'].append(condition_data)
-	return output_json
+    output_json = {"conditions": []}
+    print(model_config)
+    for condition_name, description in model_config["conditions"].items():
+        condition_data = {
+            "name": condition_name,
+            "description": description,
+            "parameters": [],
+        }
+        for param_data in model_config["parameters"]:
+            param_value = param_data["value"].get(condition_name)
+            condition_data["parameters"].append(
+                {"id": param_data["id"], "value": param_value}
+            )
+        output_json["conditions"].append(condition_data)
+    return output_json
