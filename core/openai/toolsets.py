6df831df13edc87b5e7fb4c102a913e3d9dd82ec
Julian Whiting <j2whitin@gmail.com>
2024-02-26 12:47:59 -0500

    build toolset with toolset context

--- core/openai/toolsets.py
+++ core/openai/toolsets.py
@@ -1,5 +1,27 @@
-from core.openai.tool_utils import _get_model_config, _ask_a_human
+from core.openai.tool_utils import read_csv, get_date, ask_a_human
 from core.entities import Toolset, Tool
 
-ASKEMConfigDemo = Toolset([_get_model_config, _ask_a_human])
-AskHuman = Toolset([_ask_a_human])
+
+
+with Toolset() as DatasetConfig:
+    DatasetConfig.add_tool(
+        "ask_a_human",
+        ["human_instructions"],
+        "Asks the end user for their input. Useful if there are no existing tools to solve your task. You can rely on the user to search the web, provide personal details, and generally provide you with up-to-date information.",
+        ask_a_human,
+        str,
+    )
+    DatasetConfig.add_tool(
+		"get_date",
+		["date_format"],
+		"Returns the current date.",
+		get_date,
+		str,
+	)
+    DatasetConfig.add_tool(
+		"read_csv",
+		["file_path", "**kwargs"],
+		"Reads a CSV file into a pandas DataFrame.",
+		read_csv,
+		str,
+	)
