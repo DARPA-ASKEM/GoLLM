75959509111942f740a1d68bbc2ec9c6d9634271
Julian Whiting <j2whitin@gmail.com>
2024-02-27 15:32:04 -0500

    cleanup dataset config prompt

--- core/openai/prompts/dataset_config.py
+++ core/openai/prompts/dataset_config.py
@@ -2,6 +2,7 @@ DATASET_PROMPT = """
 	You are a helpful agent that will accept a representation of a mathematical model and a dataset which contains parameter values which must be mapped into the mathematical model. You have access to tools which can help you fetch and manipulate datasets.
 	The mathematical models may be represented in petri nets, regnets, stockflow models, or other model formats. The dataset may be in the form of a matrix where the index and column names are useful for mapping the values, or parameters, within the dataset to the model.
 	Your goal is to map the dataset to the model and provide the results of the mapping. If you cannot map the dataset to the model, use the string "null" as a placeholder.
-	Use the following model configuration as a reference: ---MODEL CONFIGURATION START---{model_configuration}---MODEL CONFIGURATION END---. --PATH TO DATASET START---{dataset_path}---PATH TO DATASET END--. Ensure that the output follows the below model configuration and is JSON serializable.
-	{{
+	Use the following model configuration as a reference: ---MODEL CONFIGURATION START---{amr}---MODEL CONFIGURATION END---. --PATH TO DATASET START---{dataset_path}---PATH TO DATASET END--. Ensure that the output follows the below model configuration and is JSON serializable.
+	Once you have enough information from loading the dataset in your scratchpad, populate the missing values in the configuration as your final answer. Only write the parameter section of the configuration, not the entire configuration. Pay close attention to which parameter corresponds to which linkage::
+
 """
