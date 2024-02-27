c86f7218746109949791645c73af8e81a4b73870
Julian Whiting <j2whitin@gmail.com>
2024-02-27 15:34:49 -0500

    give react agent better instructions for CSV loading

--- core/openai/toolsets.py
+++ core/openai/toolsets.py
@@ -59,24 +59,11 @@ def download_from_presigned_url(presigned_url: str):
 ### Toolsets ###
 
 with Toolset() as DatasetConfig:
-    DatasetConfig.add_tool(
-        "ask_a_human",
-        ["human_instructions"],
-        "Asks the end user for their input. Useful if there are no existing tools to solve your task. You can rely on the user to search the web, provide personal details, and generally provide you with up-to-date information.",
-        ask_a_human,
-        str,
-    )
-    DatasetConfig.add_tool(
-		"get_date",
-		["date_format"],
-		"Returns the current date.",
-		get_date,
-		str,
-	)
+
     DatasetConfig.add_tool(
 		"read_csv",
 		["file_path", "**kwargs"],
-		"Reads a CSV file into a pandas DataFrame.",
+		"Reads a CSV file into a pandas DataFrame. This gives you access to the user\'s local machine",
 		read_csv,
 		str,
 	)
