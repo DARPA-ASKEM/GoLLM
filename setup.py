960470d7615ae06eec1ff612e3555b8e29377cfb
Julian Whiting <j2whitin@gmail.com>
2024-02-28 12:03:46 -0500

    add entry points for tasks

--- setup.py
+++ setup.py
@@ -16,6 +16,8 @@ setup(
             "gollm:configure_model=tasks.configure_model:main",
             "gollm:model_card=tasks.model_card:main",
             "gollm:embedding=tasks.embedding:main",
+			"gollm:compare_models=tasks.compare_models:main",
+			"gollm:dataset_configure=tasks.dataset_configure:main",
         ],
     },
     python_requires=">=3.8",
