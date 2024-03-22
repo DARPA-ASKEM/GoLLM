363a5820f6dfa06cc4daf3da2ab24fad9dece98f
Julian Whiting <j2whitin@gmail.com>
2024-03-22 12:22:40 -0400

    parse initial values, add type identifier in case of dupes

--- core/openai/prompts/dataset_config.py
+++ core/openai/prompts/dataset_config.py
@@ -9,14 +9,14 @@ REACT_DATASET_PROMPT = """
 """
 
 DATASET_PROMPT = """
-	You are a helpful agent that will accept a representation of a mathematical model and one or more dataset which contain parameter, or initials, values which must be mapped into the mathematical model. The mathematical models may be represented in petri nets, regnets, stockflow models, or other model formats. The datasets may be in the form of a matrix where the index and column names are useful for mapping the values, or parameters, within the dataset to the model.
+	You are a helpful agent that will accept a representation of a mathematical model and one or more dataset which contain parameter and initials values which must be mapped into the mathematical model. The mathematical models may be represented in petri nets, regnets, stockflow models, or other model formats. The datasets may be in the form of a matrix where the index and column names are useful for mapping the values of initials and parameters within the dataset to the model.
 	Your goal is to map the dataset to the model and provide the results of the mapping. If you cannot map the dataset to the model, use the string "null" as a placeholder.
 	Use the following model configuration as a reference: ---MODEL CONFIGURATION START---{amr}---MODEL CONFIGURATION END---. ---DATASETS START---{datasets}---DATASETS END--. Ensure that the output follows the below model configuration and is JSON serializable.
 	Once you have enough information from loading the dataset in your scratchpad, populate the missing values in the configuration as your final answer. Parameters should follow the format:\n
-	 parameters: [
-			{{'id': 'str', 'value': float}},
+		values: [
+			{{"id": "str", "value": float, "type": "parameter" or "initial"}},
 			...
 			]
-	Only write the parameter section of the configuration, not the entire configuration. Pay close attention to which parameter corresponds to which linkage. Do not generate markdown and ensure your output is JSON serializable:
+	Only write the ids and values of initials and parameters, do not write the entire configuration. Pay close attention to which parameter corresponds to which linkage. Do not generate markdown and ensure your output is JSON serializable:
 	OUTPUT: {{
 """
