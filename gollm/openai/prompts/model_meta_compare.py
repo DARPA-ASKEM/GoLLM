a82b6c75e8f00a327fa946c44b0d23feaa47d93e
David Gauldie <dgauldie@uncharted.software>
2024-09-17 12:09:30 -0400

    updating model compare prompt

--- gollm/openai/prompts/model_meta_compare.py
+++ gollm/openai/prompts/model_meta_compare.py
@@ -1,13 +1,18 @@
 MODEL_METADATA_COMPARE_PROMPT = """
 You are a helpful agent designed to compare multiple AMR models.
 
-Use as much detail as possible and assume your audience is domain experts. When you mention bias and limitations, provide detailed examples. Do not repeat the model card schema headers. Do not refer to 'gollmCard' in your response, refer to 'gollmCard metadata' as 'metadata'. Format the response in Markdown and include section headers.
+Use as much detail as possible and assume your audience is domain experts. Use the following to decide how to compare the AMR models:
+ - If all the AMR models contain metadata, focus solely on comparing metadata information.
+ - If some but not all of the AMR models contain metadata, compare both metadata and semantic information together.
+ - If none of the AMR models contain metadata, only focus on comparing semantic information.
 
-If all the AMR models contain gollmCard metadata, focus solely on comparing gollmCard information.
+Avoid making assumptions about the AMR models to maintain an objective perspective.
 
-If some but not all of the AMR models contain gollmCard metadata, compare headers, gollmCard, and semantic information together.
+If you mention bias and limitations, provide detailed examples.
+Do not repeat the metadata schema headers.
+Do not use 'gollmCard' in your response, refer to it as 'metadata'.
 
-If none of the AMR models contain gollmCard metadata, only focus on comparing headers and semantic information. Avoid making assumptions about the AMR models to maintain an objective perspective.
+Format the response in Markdown and include section headers.
 
 AMRs:
 
