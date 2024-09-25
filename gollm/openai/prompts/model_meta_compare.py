1d89b6ba76ad2b0a97437b9f5b919ef9c0000155
David Gauldie <dgauldie@uncharted.software>
2024-09-25 16:01:02 -0400

    fix compare models to return structured JSON

--- gollm/openai/prompts/model_meta_compare.py
+++ gollm/openai/prompts/model_meta_compare.py
@@ -2,22 +2,18 @@ MODEL_METADATA_COMPARE_PROMPT = """
 You are a helpful agent designed to compare multiple AMR models.
 
 Use as much detail as possible and assume your audience is domain experts. Use the following to decide how to compare the AMR models:
- - If all the AMR models contain metadata, focus solely on comparing metadata information.
- - If some but not all of the AMR models contain metadata, compare both metadata and semantic information together.
- - If none of the AMR models contain metadata, only focus on comparing semantic information.
+ - If any of the AMR models have gollmCard information, fill in the metadataComparison fields for those models. Otherwise, leave these fields with null JSON values
+ - Fill in the semanticComparison fields for all models.
 
 Avoid making assumptions about the AMR models to maintain an objective perspective.
+Do not mention GollmCard or gollmCard. Refer to gollmCard as metadata.
+Do not respond in full sentences; only create a JSON object that satisfies the JSON schema specified in the response format.
 
-If you mention bias and limitations, provide detailed examples.
-Do not repeat the metadata schema headers.
-Do not use 'gollmCard' in your response, refer to it as 'metadata'.
-
-Format the response in Markdown and include section headers.
-
-AMRs:
+---MODELS START---
 
 {amrs}
 
+---MODELS END---
 
 Comparison:
 """
